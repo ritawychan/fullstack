d58e634b6416a950eacfd702d048cf5fdd7d519c
Rita Chan <rita.waiyu.chan@gmail.com>
2025-12-24 11:00:09 +0100

    initial commit

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,26 +10,33 @@ const Button = (props) => {
   )
 }
 
+const Statistics = ({good, neutral, bad, all}) => {
+  const average = all === 0? 0: (good - bad)/ all
+  const positive = all === 0? 0: (good/all) *100
+
+  return (
+    <>
+      <div>Good {good}</div>
+      <div>Neutral {neutral}</div>
+      <div>Bad {bad}</div>
+      <div>All {all}</div>
+      <div>Average {average}</div>
+      <div>Positive {positive}%</div>
+    </>
+
+  )
+
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const all = good + neutral + bad
-  const average = all === 0? 0: (good - bad)/ all
-  const positive = all === 0? 0: (good/all) *100
 
-  const handleGoodClick = () => {
-    setGood(good +1)
-  }
-
-  const handleNeutralClick = () => {
-    setNeutral(neutral +1)
-  }
-
-  const handleBadClick = () => {
-    setBad(bad +1)
-  }
+  const handleGoodClick = () => setGood(good +1)
+  const handleNeutralClick = () => setNeutral(neutral +1)
+  const handleBadClick = () => setBad(bad +1)
 
   return (
     <>
@@ -39,13 +46,12 @@ const App = () => {
       <Button onClick = {handleNeutralClick} text = 'Neutral'/>
       <Button onClick = {handleBadClick} text = 'Bad'/>
       </div>
-      <h1>Statistics</h1>
-      <div>Good {good}</div>
-      <div>Neutral {neutral}</div>
-      <div>Bad {bad}</div>
-      <div>All {all}</div>
-      <div>Average {average}</div>
-      <div>Positive {positive}%</div>
+      <div>
+        <h1>Statistics</h1>
+      {all === 0 ? <p> No feedback</p>:
+        <Statistics good ={good} neutral = {neutral} bad ={bad}  all = {all}/>
+      }
+      </div>
     </>
   )
 }
