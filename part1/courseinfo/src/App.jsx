bb43f01884429f1cfe4c3ad02b29e6eac4a1c4e1
Rita Chan <rita.waiyu.chan@gmail.com>
2025-12-27 21:38:54 +0100

    2.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,11 +4,15 @@ const Content = ({parts}) => (parts.map(part => <Part key={part.id} part = {part
 
 const Part = ({part}) => <div>{part.name} {part.exercises}</div>
 
+const Total = ({parts}) => <div> Total of {(parts.reduce((total, part) =>  total + part.exercises, 0))} exercises </div>
+
 const Course = ({course}) => {
+
   return (
     <>
       <Header course = {course}/>
       <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
     </>
   )
 }
@@ -32,7 +36,12 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
-      }
+      },
+      {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
     ]
   }
 
